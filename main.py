5f1c9d4497fb48df8a639b617111b17e86a00761
madwichery <aurizdwic@gmail.com>
2025-07-06 07:26:54 +0700

    add accept_text_as_string_return_number_of_words and {num_words} words found in the document

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ def get_book_text(path_to_file):
         file_contents = f.read()
     return file_contents
 
+def accept_text_as_string_return_number_of_words(text):
+    words = text.split()
+    num_words = (len(words))
+    return num_words
+
 def main():
-    print(get_book_text(path_to_file))
+    text_from = get_book_text(path_to_file)
+    num_words = accept_text_as_string_return_number_of_words(text_from)
+    print(f"{num_words} words found in the document")
 
 if __name__ == "__main__":
     main()
