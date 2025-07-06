3589be27eeae9e7f0a16dc587290a5fa2b3dd266
madwichery <aurizdwic@gmail.com>
2025-07-06 07:38:20 +0700

    change name function to get_num_words and refactor

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from stats import get_num_words
 path_to_file = "/home/akutokyo/workspace/github.com/madwichery/bookbot/books/frankenstein.txt"
 
 def get_book_text(path_to_file):
@@ -5,14 +6,9 @@ def get_book_text(path_to_file):
         file_contents = f.read()
     return file_contents
 
-def accept_text_as_string_return_number_of_words(text):
-    words = text.split()
-    num_words = (len(words))
-    return num_words
-
 def main():
     text_from = get_book_text(path_to_file)
-    num_words = accept_text_as_string_return_number_of_words(text_from)
+    num_words = get_num_words(text_from)
     print(f"{num_words} words found in the document")
 
 if __name__ == "__main__":
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_num_words(text):
+    words = text.split()
+    num_words = (len(words))
+    return num_words
