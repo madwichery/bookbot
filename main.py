ca17113e73d9ecbb1a69627699b55e625ff4738b
madwichery <aurizdwic@gmail.com>
2025-07-07 17:45:33 +0700

    add function counting_characters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, counting_characters
 path_to_file = "/home/akutokyo/workspace/github.com/madwichery/bookbot/books/frankenstein.txt"
 
 def get_book_text(path_to_file):
@@ -10,6 +10,9 @@ def main():
     text_from = get_book_text(path_to_file)
     num_words = get_num_words(text_from)
     print(f"{num_words} words found in the document")
+
+    char_counts = counting_characters(text_from)
+    print(char_counts)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -2,3 +2,13 @@ def get_num_words(text):
     words = text.split()
     num_words = (len(words))
     return num_words
+
+def counting_characters(text):
+    char_counts = {}
+    for character in text:
+        lower_case_character = character.lower()
+        if lower_case_character in char_counts:
+            char_counts[lower_case_character] += 1
+        else:
+            char_counts[lower_case_character] = 1    
+    return char_counts
